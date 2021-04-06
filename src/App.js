7c659f150ef4cc5edd86eb91b3a67d94281e8924
shoheb <shohebtasewale9623@gmail.com>
2021-04-06 16:01:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ function App() {
 
   useEffect(() => {
     dictionaryApi();
+    // eslint-disable-next-line
   }, [word, category]);
 
   return (
--- src/Components/Definitions/Definitions.js
+++ src/Components/Definitions/Definitions.js
@@ -7,7 +7,7 @@ const Definitions = ({ word, meanings, category, lightMode }) => {
 
 
             {
-                meanings[0] && word && category == 'en' && (
+                meanings[0] && word && category === 'en' && (
                     <audio
                         src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
                         style={{ backgroundColor: '#fff', borderRadius: 10 }}
